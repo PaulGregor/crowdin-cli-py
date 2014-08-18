10f4c7e890769a506c689d16ec1f3641686489ad
root <comixan@gmail.com>
2014-08-18 08:52:56 +0000

    test #1

--- connection.py
+++ connection.py
@@ -1,19 +1,24 @@
 # -*- coding: utf-8 -*-
-__author__ = 'Comix'
 import yaml, requests, json
-
+import logging
 
 LOCATION_TO_CONFIGURATION_FILE = 'crowdin.yaml'
 ACCOUNT_KEY = ''
 USER_NAME = ''
 
+logger = logging.getLogger('crowdin')
+
+
+class CliException(Exception):
+    pass
+
 
 class Configuration(object):
-    def __init__(self, conf_file, account_key=None):
+    def __init__(self):
 
         # reading configuration file
         try:
-            fh = open(conf_file, "r")
+            fh = open(LOCATION_TO_CONFIGURATION_FILE, "r")
             config = yaml.load(fh)
         except(OSError, IOError) as e:
             print e, "\n Please check your config file"
@@ -23,15 +28,15 @@ class Configuration(object):
 
             fh.close()
 
-            #assigning configuration values
-           # print "Reading configuration from the file was successful"
+            # assigning configuration values
+            # print "Reading configuration from the file was successful"
             self.project_identifier = config['project_identifier']
             self.api_key = config['api_key']
             self.base_url = config['base_url']
             self.base_path = config['base_path']
-            self.files_source = config['files'][0]['source']
-
-            self.account_key = account_key
+            #self.files_source = config['files'][0]['source']
+            self.files_source = config['files']
+            self.account_key = ACCOUNT_KEY
 
     def get_project_identifier(self):
         return self.project_identifier
@@ -45,37 +50,39 @@ class Configuration(object):
     def get_base_path(self):
         return self.base_path
 
-    def get_account_key(self):
-        return self.account_key
+    def get_files_source(self):
+        sources = []
+        for f in self.files_source:
+            sources.append(f['source'])
+        return sources
 
 
 class Connection(Configuration):
     def __init__(self, url, params):
-        super(Connection, self).__init__(LOCATION_TO_CONFIGURATION_FILE, ACCOUNT_KEY)
+        super(Connection, self).__init__()
         self.url = url
         self.params = params
 
     def connect(self):
-        valid_url = self.base_url + self.url['url_par1'] + self.get_project_identifier() + self.url['url_par3'] + '?key=' + self.get_api_key()
+        valid_url = self.base_url + self.url['url_par1'] + self.get_project_identifier() + self.url[
+            'url_par3'] + '?key=' + self.get_api_key()
 
-        f = requests.request(self.url['post'], valid_url,  data=self.params)
-        return result_handling(f.text)
+        response = requests.request(self.url['post'], valid_url, data=self.params)
+        if response.status_code != 200:
+            return result_handling(response.text)
+        #    raise CliException(response.text)
+        else:
+            return response.text
+        #return response.text
 
 
 def result_handling(self):
     data = json.loads(self)
-    #msg = "Operation was {0}".format()
+    # msg = "Operation was {0}".format()
     if data["success"] is False:
-
-        return "Operation was unsuccessful. \nError code: {0}. Error message: {1}".format(data["error"]["code"], data["error"]["message"])
+        #raise CliException(self)
+        logger.info("Operation was unsuccessful")
+        print "Error code: {0}. Error message: {1}".format(data["error"]["code"], data["error"]["message"])
     else:
-
-        return "Operation was successful"
-
-
-
-
-
-
-
-
+        logger.info("Operation was successful")
+        print self
