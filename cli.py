a50a12f908fd04ce34714e26d92facaf9f075c2f
root <root@d1stkfactory>
2014-08-18 08:46:08 +0000

    way better way..

--- cli.py
+++ cli.py
@@ -1,96 +1,104 @@
 import argparse
 import gettext
 import methods
+import logging
+import sys, os
+
+
+level = logging.DEBUG
+formatter = logging.Formatter('%(message)s')
+logger = logging.getLogger('crowdin')
+logger.setLevel(level)
+
+console = logging.StreamHandler()
+console.setLevel(level)
+console.setFormatter(formatter)
+
+logger.addHandler(console)
 
 
 def create_directory(string):
-
     print methods.Methods().create_directory(string.dirname, string.json, string.jsonp)
 
 
-def add_file(string):
+def upload_files(string):
+    if string.sources == "sources":
+        print methods.Methods().upload_sources()
 
-    print methods.Methods().add_file(string.dirname, string.json, string.jsonp)
+
+        # print methods.Methods().upload_files(string.dirname, string.json, string.jsonp)
 
 
+def download_project(string):
+    pass
 
 
 loc = gettext.translation('cli', 'locales', languages=['en'])
-_= loc.ugettext
+_ = loc.ugettext
 loc.install()
-
 
 parser = argparse.ArgumentParser(description=_("desc"), )
 
 subparsers = parser.add_subparsers(help='commands')
 
-# A list command
-list_parser = subparsers.add_parser('list', help='List contents')
-list_parser.add_argument('dirname', action='store', help='Directory to list')
+# A upload command
+upload_parser = subparsers.add_parser('upload', help='Upload filles to the server')
+upload_parser.add_argument('sources', action='store', help='Sources filles', nargs='?')
+upload_parser.add_argument('translations', action='store', help='Translations filles', nargs='?')
+
+upload_parser.set_defaults(func=upload_files)
+
+
+# A download command
+download_parser = subparsers.add_parser('download', help='Download projects filles')
+
+download_parser.set_defaults(func=download_project)
+
+
 
 # A create command
 create_parser = subparsers.add_parser('create', help='Add directory to Crowdin project.')
 create_parser.add_argument('dirname', action='store', help='New directory to create')
-create_parser.add_argument('--json', default=False, action='store_true',
-                           help='May not contain value. Defines that response should be in JSON format.',
-                           )
-create_parser.add_argument('--jsonp', default=False, action='store_true',
-                           help='Callback function name. Defines that response should be in JSONP format.',
-                           )
+
 create_parser.set_defaults(func=create_directory)
 
 
-# Add file command
-add_parser = subparsers.add_parser('addfiles', help='Add new file to Crowdin project.')
-add_parser.add_argument('files', action='store', help='Files array that should be added to Crowdin project.')
-add_parser.add_argument('--titles', default=False, action='store_true', help='An arrays of strings that defines titles for uploaded files.')
-add_parser.add_argument('--export_patterns', default=False, action='store_true', help='An arrays of strings that defines names of resulted files.')
-add_parser.add_argument('--type', default=False, action='store_true', help='')
-add_parser.add_argument('--first_line_contains_header', default=False, action='store_true', help='')
-add_parser.add_argument('scheme', action='store', help='')
-
-add_parser.add_argument('--json', default=False, action='store_true',
-                           help='May not contain value. Defines that response should be in JSON format.',
-                           )
-add_parser.add_argument('--jsonp', default=False, action='store_true',
-                           help='Callback function name. Defines that response should be in JSONP format.',
-                           )
-add_parser.add_argument('--translate_content', default=1, action='store_true',
-                           help='Defines whether to translate texts placed inside the tags. Acceptable values are: 0 or 1. Default is 1.',
-                           )
-add_parser.add_argument('--translate_attributes', default=1, action='store_true',
-                           help='Defines whether to translate tags attributes. Acceptable values are: 0 or 1. Default is 1.',
-                           )
-add_parser.add_argument('--content_segmentation', default=1, action='store_true',
-                           help='Defines whether to split long texts into smaller text segments.',
-                           )
-add_parser.add_argument('--translatable_elements', default=1, action='store_true',
-                           help='This is an array of strings, where each item is the XPaths to DOM element that should be imported.',
-                           )
-add_parser.set_defaults(func=add_file)
-
-
-
-
-# A rename command
-rename_parser = subparsers.add_parser('rename', help='Rename directory or modify its attributes.')
-rename_parser.add_argument('dirname', action='store', help='New directory to create')
-
-
 # A delete command
-delete_parser = subparsers.add_parser('delete', help='Remove a directory')
-delete_parser.add_argument('dirname', action='store', help='The directory to remove')
-delete_parser.add_argument('--recursive', '-r', default=False, action='store_true',
-                           help='Remove the contents of the directory, too',
-                           )
+# delete_parser = subparsers.add_parser('delete', help='Remove a directory')
+# delete_parser.add_argument('dirname', action='store', help='The directory to remove')
+# delete_parser.add_argument('--recursive', '-r', default=False, action='store_true',
+# help='Remove the contents of the directory, too',
+#                            )
+# create_parser.add_argument('--json', default=False, action='store_true',
+#                            help='May not contain value. Defines that response should be in JSON format.',
+# )
+# create_parser.add_argument('--jsonp', default=False, action='store_true',
+#                            help='Callback function name. Defines that response should be in JSONP format.',
+# )
 
 
-#parser.add_argument('-o','--output',help='Output file name', required=True)
-args = parser.parse_args()
-args.func(args)
-## show values ##
 
-# if args.create == "install":
-#     print "You asked for installation"
-# else:
-#     print "usage: cli.py [-h]"
+
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    args.func(args)
+    ## show values ##
+
+
+    # if args == "version":
+    #     print sys.stdout.write("crowdin-client 0.1\n" )
+
+    # if args == "no way":
+    #     level = logging.INFO
+    #     formatter = logging.Formatter('%(levelname)s: %(message)s')
+    # else:
+
+
+
+
+    # if args.create == "install":
+    #     print "You asked for installation"
+    # else:
+    #     print "usage: cli.py [-h]"
--- methods.py
+++ methods.py
@@ -1,14 +1,104 @@
-# -*- coding: utf-8 -*-
+﻿# -*- coding: utf-8 -*-
 __author__ = 'Comix'
-from connection import Connection
+from connection import Connection, Configuration
+import logging
+import json
+import os
+
+logger = logging.getLogger('crowdin')
+
 
 
 class Methods:
 
 
-    def create_directory(self, name, json, jsonp):
+    def get_info(self):
+        #POST https://api.crowdin.com/api/project/{project-identifier}/info?key={project-key}
+        url = {'post': 'POST', 'url_par1': '/api/project/', 'url_par3': '/info'}
+        params = {'json': 'json'}
+        data = json.loads(Connection(url, params).connect())
+
+        return data['files']
+
+    def exists(self, name):
+
+        for f in name:
+            for i in f.split():
+                yield i
+
+
+
+    # def test(self, tre):
+    #     for i in tre:
+    #         print u'├─ ' + i['name']
+    #         if isinstance(i, dict):
+    #             item = self.test(i['files'])
+    #             if item is not None:
+    #                 return item
+    #for index, elm in enumerate(f.split('/')):
+
+    def parse(self, data, parent=''):
+        if data is None or not len(data):
+            yield parent + ('/' if data is not None and not len(data) else '')
+        else:
+            for node in data:
+                for result in self.parse(
+                        node.get('files'), parent + '/' + node.get('name')):
+                    yield result
+
+
+    def create_directory(self, name):
         # POST https://api.crowdin.net/api/project/{project-identifier}/add-directory?key={project-key}
 
+
+        logger.info("Creating remote directory {0}".format(name))
+
+
         url = {'post': 'POST', 'url_par1': '/api/project/', 'url_par3': '/add-directory'}
-        params = {'name': name, 'json': json, 'jsonp': jsonp}
+        params = {'name': name, 'json': json}
         return Connection(url, params).connect()
+
+    def upload_sources(self):
+        dirs = []
+
+        for item in Methods().parse(Methods().get_info()):
+            l=[]
+            p = "/"
+
+            f = item[:item.rfind("/")]
+            l = f[1:].split("/")
+            i = 0
+            while i < len(l):
+                p = p + l[i] + "/"
+                i = i + 1
+                if not p in dirs:
+                    dirs.append(p)
+
+
+        for item in self.exists(Configuration().get_files_source()):
+            if not item[:item.rfind("/")] in dirs:
+                items = item[:item.rfind("/")]
+                l=[]
+                p = "/"
+                l = items[1:].split("/")
+                i = 0
+                while i < len(l):
+                    p = p + l[i] + "/"
+                    i = i + 1
+                    if not p in dirs:
+                        self.create_directory(p)
+
+
+
+
+
+                #self.create_directory(item[:item.rfind("/")])
+       #print dirs
+
+
+
+
+
+
+#Methods().upload_sources()
+#Methods().create_directory("/locale/fr/LC_MESSAGES/")
