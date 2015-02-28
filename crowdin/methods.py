77eaf71726dc182f7d54a4b41cc415c988046da4
Paul <comixan@gmail.com>
2015-02-28 18:42:43 +0200

    Update methods.py

--- crowdin/methods.py
+++ crowdin/methods.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from __future__ import division, absolute_import, print_function, unicode_literals
+from __future__ import division, print_function, unicode_literals
 import six
 from connection import Connection, Configuration
 import logging
