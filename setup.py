9dc2aa275b300a0516c618fc862137a5b4d4bd8b
Slach <bloodjazman@gmail.com>
2016-06-02 23:18:38 +0300

    draft worked version of Versions workflow
    Signed-off-by: Slach <bloodjazman@gmail.com>

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('requirements.txt') as reqs:
 
 setup(
     name='crowdin-cli-py',
-    version='0.95.2',
+    version='0.95.4',
     author='PaulGregor',
     author_email='comixan@gmail.com',
     packages=['crowdin'],
