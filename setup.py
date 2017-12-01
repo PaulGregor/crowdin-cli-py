58f6d55983c5405f39a31301f823968a3dcc9855
NikitaMorev <n.morev@melsoft-games.com>
2017-12-01 18:02:15 +0300

    HotFix for osx_code

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('requirements.txt') as reqs:
 
 setup(
     name='crowdin-cli-py',
-    version='0.95.5',
+    version='0.95.6',
     author='PaulGregor',
     author_email='comixan@gmail.com',
     packages=['crowdin'],
